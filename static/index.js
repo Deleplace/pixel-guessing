7275a28362ddf5828ab27866141a21514325e0d9
Deleplace <deleplace@google.com>
2024-01-19 15:01:56 +0100

    Factor code: single function startGuessing.

--- static/index.js
+++ static/index.js
@@ -12,8 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-function startGuessingSample(sample, naturalWidth) {
-    console.log("Start guessing", sample, "having original width", naturalWidth);
+// Expecting namedParam to have the form
+// either "sample=samples/sample18.jpg"
+// or     "imgid=y71q"
+// so the same code works with either a sample picture, or a user-provided picture.
+function startGuessing(namedParam, naturalWidth) {
+    console.log("Start guessing", namedParam, "having original width", naturalWidth);
     aiconversation.innerHTML = `
         <tr>
             <th> </th>
@@ -25,12 +29,12 @@ function startGuessingSample(sample, naturalWidth) {
     let i = 0;
     for(let pixelW=8; pixelW<=naturalWidth; pixelW*=1.3) {
         let w = Math.floor(pixelW);
-        setTimeout((sample, i) => {
+        setTimeout((namedParam, i) => {
             let row = document.createElement('tr');
             row.innerHTML = `
                 <td>
                     <span class="resolution" id="resolution-${i}">${w}</span>
-                    <img src="/resized?sample=${sample}&pixelwidth=${w}" id="resized-${i}" />
+                    <img src="/resized?${namedParam}&pixelwidth=${w}" id="resized-${i}" />
                 </td>
                 <td class="answer" id="answer-${i}">
                     <div class="loading">Let me guess... </div>
@@ -44,51 +48,8 @@ function startGuessingSample(sample, naturalWidth) {
                 // When the pixelated image arrives, show its resolution w x h (in pixels)
                 resolution.innerHTML = `${img.naturalWidth}x${img.naturalHeight}`;
             };
-            guessSingle(`/guess?sample=${sample}&pixelwidth=${w}`, i);
-        }, 2000 * i, sample, i);
-        i++;
-    }
-}
-
-
-// Call startGuessingUserImage() after successful upload
-// This is mostly the same code as startGuessingSample, but using the image ID
-// of a recently uploaded user image.
-// TODO merge this code with startGuessingSample ?
-function startGuessingUserImage(imageID, naturalWidth) {
-    console.log("Start guessing", imageID, "having original width", naturalWidth);
-    aiconversation.innerHTML = `
-        <tr>
-            <th> </th>
-            <th class="prompt">
-                What does this picture look like? Provide a short answer in less than 8 words.
-            </th>
-        </tr>
-        `;
-    let i = 0;
-    for(let pixelW=8; pixelW<=naturalWidth; pixelW*=1.3) {
-        let w = Math.floor(pixelW);
-        setTimeout((imgID, i) => {
-            let row = document.createElement('tr');
-            row.innerHTML = `
-                <td>
-                    <span class="resolution" id="resolution-${i}">${w}</span>
-                    <img src="/resized?imgid=${imgID}&pixelwidth=${w}" id="resized-${i}" />
-                </td>
-                <td class="answer" id="answer-${i}">
-                    <div class="loading">Let me guess... </div>
-                    <div class="loading">(asking the Gemini Pro Vision model with Vertex AI)</div>
-                </td>
-            `;
-            aiconversation.appendChild(row);
-            let img = document.getElementById(`resized-${i}`);
-            let resolution = document.getElementById(`resolution-${i}`);
-            img.onload = () => {
-                // When the pixelated image arrives, show its resolution w x h (in pixels)
-                resolution.innerHTML = `${img.naturalWidth}x${img.naturalHeight}`;
-            };
-            guessSingle(`/guess?imgid=${imageID}&pixelwidth=${w}`, i);
-        }, 2000 * i, imageID, i);
+            guessSingle(`/guess?${namedParam}&pixelwidth=${w}`, i);
+        }, 2000 * i, namedParam, i);
         i++;
     }
 }
@@ -157,7 +118,7 @@ up.addEventListener('change', () => {
         return response.json();
     })
     .then(data => {
-        startGuessingUserImage(data.imageID, data.width);
+        startGuessing("imgid=" + data.imageID, data.width);
     })
     .catch(err => {
         // TODO
@@ -182,6 +143,6 @@ sampleImages.forEach(image => {
         image.classList.add('selected');
         let sample = image.getAttribute('src');
 
-        startGuessingSample(sample, image.naturalWidth);
+        startGuessing("sample=" + sample, image.naturalWidth);
     });
 });
